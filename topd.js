6f29259e350bc0425706b0fdf7e5c5b1f3c3be37
Cesar <cesaroach@gmail.com>
2017-05-11 17:48:48 -0300

    Filter self domain, twitter and github

--- topd.js
+++ topd.js
@@ -3,23 +3,38 @@ let Promise = this.Promise || require('promise');
 let agent = require('superagent-promise')(request, Promise);
 let subReddit = process.argv[2];
 
-
 function getSubPosts(sub) {
   return (agent
     .get('http://reddit.com/r/'+sub+'/hot.json')
-    .query({ t: 'week', limit: 10 })
+    .query({ t: 'week', limit: 20 })
     .set('User-Agent', 'Subfinder bot (by /u/SarasaNews)')
     .end()
     .then(function(res) {
-      if (typeof(res.body.data) == 'undefined' || res.body.data.children == "") {
+    	if (res.body.data.children == "") {
         console.log('There is no subreddit with this name')
         return;
-      }
-      for (let i in res.body.data.children) {
-	      console.log(res.body.data.children[i].data.url);
-      }
-    }));
+      	}
+    	let SelfDomain = res.body.data.children.filter(selfPost);
+    	let TwitterDomain = SelfDomain.filter(twitter);
+    	let cleanDomain = TwitterDomain.filter(github);
+      	for (let i in cleanDomain) {
+	      let domain = cleanDomain[i].data.domain;
+	      console.log(domain);
+      	}
+    })
+    .catch(console.log.bind(console))
+    );
+}
+
+//-----------filters-----------
+function selfPost(post) {
+	return post.data.domain !== "self.PHP";
+}
+function twitter(post) {
+	return post.data.domain !== "twitter.com";
+}
+function github(post) {
+	return post.data.domain !== "github.com";
 }
 
 getSubPosts(subReddit);
-
